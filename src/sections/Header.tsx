3f1458b6394545c0a435f1c918fa19b0fdc325e1
Md Yusuf <mdyusuf742171@gmail.com>
2024-11-13 20:55:41 +0530

    create nav items for lg amd md screen

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,11 +6,19 @@ const Header = () => {
   return (
     <header className="py-4 border-b border-white/15">
       <div className="container">
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <LogoIcon className="h-8 w-8" />
             </div>
+          </div>
+          <div>
+            <nav className="flex gap-8">
+              <a className="text-white/70 hover:text-white transition" href="">Features</a>
+              <a className="text-white/70 hover:text-white transition" href="">Developers</a>
+              <a className="text-white/70 hover:text-white transition" href="">Pricing</a>
+              <a className="text-white/70 hover:text-white transition" href="">changelog</a>
+            </nav>
           </div>
           <div className="flex gap-4 items-center">
             <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
