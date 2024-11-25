9dbb5513998f554cf592af65a95b5c88f0d0ece6
Md Yusuf <mdyusuf742171@gmail.com>
2024-11-25 11:11:10 +0530

    finish hero section and start logoticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,37 @@
-export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+import React from "react";
+import acmeLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import echoLogo from "@/assets/logo-echo.png";
+
+const LogoTicker = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="flex">
+          <div className="flex-1">
+            <h2>Trusted by top innovative teams</h2>
+          </div>
+          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <div className="flex flex-none gap-14">
+              {[
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestialLogo,
+                apexLogo,
+                quantumLogo,
+              ].map((logo) => (
+                <img src={logo.src} key={logo.src} className="h-6 w-auto" />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
+
+export default LogoTicker;
