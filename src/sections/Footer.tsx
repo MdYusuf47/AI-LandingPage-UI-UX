deb83cd918756440accb32a4b1af82d429142a02
Md Yusuf <mdyusuf742171@gmail.com>
2024-11-28 10:45:51 +0530

    finish footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,24 +6,26 @@ import YTSocial from "@/assets/social-youtube.svg";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <Logo />
-        <div>AI Startup Landing Page</div>
-      </div>
-      <div>
-        <nav>
-          <a href="#">Features</a>
-          <a href="#">Developers</a>
-          <a href="#">Company</a>
-          <a href="#">Blog</a>
-          <a href="#">Change log</a>
-        </nav>
-      </div>
-      <div>
-        <XSocial />
-        <InstaSocial />
-        <YTSocial />
+    <footer className="py-4 border-t border-white/15">
+      <div className="container">
+        <div className="flex flex-col lg:flex-row lg:items-center gap-8">
+          <div className="flex gap-2 items-center lg:flex-1">
+            <Logo className='h-6 w-6'/>
+            <div className="font-medium">AI Startup Landing Page</div>
+          </div>
+            <nav className="flex flex-col lg:flex-row gap-5 lg:gap-7 lg:flex-1 lg:justify-center">
+              <a href="#" className="text-white/70 hover:text-white  md:text-sm transition">Features</a>
+              <a href="#" className="text-white/70 hover:text-white  md:text-sm transition">Developers</a>
+              <a href="#" className="text-white/70 hover:text-white  md:text-sm transition">Company</a>
+              <a href="#" className="text-white/70 hover:text-white  md:text-sm transition">Blog</a>
+              <a href="#" className="text-white/70 hover:text-white  md:text-sm transition">Changelog</a>
+            </nav>
+          <div className="flex gap-5 lg:flex-1 lg:justify-end">
+            <XSocial className='text-white/40 hover:text-white transition' />
+            <InstaSocial className='text-white/40 hover:text-white transition' />
+            <YTSocial className='text-white/40 hover:text-white transition' />
+          </div>
+        </div>
       </div>
     </footer>
   );
