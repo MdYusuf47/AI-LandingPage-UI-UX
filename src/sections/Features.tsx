3526ec0491546ec4d66abef93c5fb35732b0beef
Md Yusuf <mdyusuf742171@gmail.com>
2024-11-29 10:23:35 +0530

    add animation to logo in features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { DotLottiePlayer } from "@dotlottie/react-player";
+import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from "@/assets/product-image.png";
+import { useRef } from "react";
 
 const tabs = [
   {
@@ -31,6 +32,32 @@ const tabs = [
   },
 ];
 
+export const FeatureTab = (tab: (typeof tabs)[number]) => {
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null)
+  const handleTabHover =() =>{
+    if (dotLottieRef.current === null) return;
+    dotLottieRef.current.seek(0);
+    dotLottieRef.current.play()
+  }
+  return (
+    <div
+    onMouseEnter={handleTabHover}
+      key={tab.title}
+      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
+    >
+      <div className="h-12 w-12 border-white/15 border-lg inline-flex items-center justify-center">
+        <DotLottiePlayer ref={dotLottieRef} src={tab.icon} className="h-5 w-5" autoplay />
+      </div>
+      <div className="font-medium">{tab.title}</div>
+      {tab.isNew && (
+        <div className="text-x5 rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
+          new
+        </div>
+      )}
+    </div>
+  );
+};
+
 export const Features = () => {
   return (
     <section className="py-20 md:py-24">
@@ -44,19 +71,16 @@ export const Features = () => {
         </p>
         <div className="mt-10 flex flex-col gap-3">
           {tabs.map((tab) => (
-            <div key={tab.title} className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1">
-              <div className="h-12 w-12 border-white/15 border-lg inline-flex items-center justify-center">
-                <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay />
-              </div>
-              <div className="font-medium">{tab.title}</div>
-              {tab.isNew && <div className="text-x5 rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>}
-            </div>
+            <FeatureTab {...tab} key={tab.title} />
           ))}
         </div>
         <div className="border border-white/20 p-2.5 rounded-xl mt-3">
-        <div className="aspect-video bg-cover border border-white/20 rounded-lg" style={{backgroundImage:`url(${productImage.src})`}}>
-          {/* <Image src={productImage} alt="Product image" /> */}
-        </div>
+          <div
+            className="aspect-video bg-cover border border-white/20 rounded-lg"
+            style={{ backgroundImage: `url(${productImage.src})` }}
+          >
+            {/* <Image src={productImage} alt="Product image" /> */}
+          </div>
         </div>
       </div>
     </section>
