53daf73c93a777248301d70a06742327bfc466f3
Md Yusuf <mdyusuf742171@gmail.com>
2024-11-30 09:56:49 +0530

    add animation into features sections

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,9 +1,19 @@
 "use client";
 
-import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
+import {
+  DotLottieCommonPlayer,
+  DotLottiePlayer,
+} from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from "@/assets/product-image.png";
-import { useRef } from "react";
+import { ComponentPropsWithRef, useEffect, useRef, useState } from "react";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationTransition,
+} from "framer-motion";
 
 const tabs = [
   {
@@ -32,24 +42,66 @@ const tabs = [
   },
 ];
 
-export const FeatureTab = (tab: (typeof tabs)[number]) => {
-  const dotLottieRef = useRef<DotLottieCommonPlayer>(null)
-  const handleTabHover =() =>{
+export const FeatureTab = (
+  props: (typeof tabs)[number] &
+    ComponentPropsWithRef<"div"> & { selected: boolean }
+) => {
+  const tabRef = useRef<HTMLDivElement>(null);
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+  const xPercentage = useMotionValue(0);
+  const yPercentage = useMotionValue(0);
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%,black,transparent)`;
+  useEffect(() => {
+    if (!tabRef.current || !props.selected) return;
+    xPercentage.set(0);
+    yPercentage.set(0);
+    const { height, width } = tabRef.current?.getBoundingClientRect();
+    const circumference = height * 2 + width * 2;
+    const times = [
+      0,
+      width / circumference,
+      (width + height) / circumference,
+      (width * 2 + height) / circumference,
+      1,
+    ];
+    const options: ValueAnimationTransition = {
+      times,
+      duration: 4,
+      repeat: Infinity,
+      ease: "linear",
+      repeatType: "loop",
+    };
+    animate(xPercentage, [0, 100, 100, 0, 0], options);
+    animate(yPercentage, [0, 0, 100, 100, 0], options);
+  }, [props.selected]);
+  const handleTabHover = () => {
     if (dotLottieRef.current === null) return;
     dotLottieRef.current.seek(0);
-    dotLottieRef.current.play()
-  }
+    dotLottieRef.current.play();
+  };
   return (
     <div
-    onMouseEnter={handleTabHover}
-      key={tab.title}
-      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
+      ref={tabRef}
+      onMouseEnter={handleTabHover}
+      className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative"
+      onClick={props.onClick}
     >
+      {props.selected && (
+        <motion.div
+          style={{ maskImage }}
+          className="absolute inset-0 -m-px border border-[#A369FF] rounded-xl"
+        ></motion.div>
+      )}
       <div className="h-12 w-12 border-white/15 border-lg inline-flex items-center justify-center">
-        <DotLottiePlayer ref={dotLottieRef} src={tab.icon} className="h-5 w-5" autoplay />
+        <DotLottiePlayer
+          ref={dotLottieRef}
+          src={props.icon}
+          className="h-5 w-5"
+          autoplay
+        />
       </div>
-      <div className="font-medium">{tab.title}</div>
-      {tab.isNew && (
+      <div className="font-medium">{props.title}</div>
+      {props.isNew && (
         <div className="text-x5 rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
           new
         </div>
@@ -59,28 +111,65 @@ export const FeatureTab = (tab: (typeof tabs)[number]) => {
 };
 
 export const Features = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const backgroundPositionX = useMotionValue(tabs[0].backgroundPositionX);
+  const backgroundPositionY = useMotionValue(tabs[0].backgroundPositionY);
+  const backgroundSizeX = useMotionValue(tabs[0].backgroundSizeX);
+  const backgroundPosition = useMotionTemplate`${backgroundPositionX}% ${backgroundPositionY}%`;
+  const backgroundSize = useMotionTemplate`${backgroundSizeX}% auto`;
+  const handleSelectTab = (index: number) => {
+    setSelectedTab(index);
+    const animateOptions: ValueAnimationTransition = {
+      duration: 2,
+      ease: "easeInOut",
+    };
+    animate(
+      backgroundSizeX,
+      [backgroundSizeX.get(), 100, tabs[index].backgroundSizeX],
+      animateOptions
+    );
+    animate(
+      backgroundPositionX,
+      [backgroundPositionX.get(), tabs[index].backgroundSizeX],
+      animateOptions
+    );
+    animate(
+      backgroundPositionY,
+      [backgroundPositionY.get(), tabs[index].backgroundPositionY],
+      animateOptions
+    );
+  };
   return (
     <section className="py-20 md:py-24">
       <div className="container">
         <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
           Elevate your SEO efforts
         </h2>
-        <p className="text-white/70 text-lg md:text-xl max-w-2xl max-auto tracking-tight text-center mt-5">
+        <p className="text-white/70 text-lg md:text-xl max-w-2xl max-auto tracking-tight text-center mt-5 lg:px-5">
           From small startup to large enterprises, our AI-driven tool has
           revolutionize the way businesses approach SEO.
         </p>
         <div className="mt-10 flex flex-col gap-3">
-          {tabs.map((tab) => (
-            <FeatureTab {...tab} key={tab.title} />
+          {tabs.map((tab, tabIndex) => (
+            <FeatureTab
+              {...tab}
+              selected={selectedTab === tabIndex}
+              onClick={() => handleSelectTab(tabIndex)}
+              key={tab.title}
+            />
           ))}
         </div>
         <div className="border border-white/20 p-2.5 rounded-xl mt-3">
-          <div
+          <motion.div
             className="aspect-video bg-cover border border-white/20 rounded-lg"
-            style={{ backgroundImage: `url(${productImage.src})` }}
+            style={{
+              backgroundPosition,
+              backgroundSize,
+              backgroundImage: `url(${productImage.src})`,
+            }}
           >
             {/* <Image src={productImage} alt="Product image" /> */}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
